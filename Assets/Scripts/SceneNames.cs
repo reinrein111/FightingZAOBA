f60f1a65f290475c186bc368a7f7f72af6df862b
Surpass567 <2918912761@qq.com>
2026-04-03 13:25:31 +0800

    碰撞箱，镜头分割，人物跳跃

--- Assets/Scripts/SceneNames.cs
+++
@@ -1,8 +0,0 @@
-public static class SceneNames
-{
-    public const string MAIN_MENU = "MainMenu";
-    public const string LOADING = "LoadingScene";
-    public const string MAP_0 = "ZAOBA_Map_0";
-    public const string MAP_1 = "ZAOBA_Map_1";
-    public const string MAP_2 = "ZAOBA_Map_2";
-}
